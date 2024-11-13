d0e453f3bda36297cf1e27cca837dfe3a5ac4696
Fernanda Souza de Oliveira <fersouzaoliveira@gmail.com>
2024-11-13 16:02:20 -0300

    Update te_age.R

--- te_age.R
+++ te_age.R
@@ -1,37 +1,39 @@
 # Median terminal branch length
+library(nortest)
+library(FSA)
 library (ggplot2)
 library(dplyr)
-library(multcompView)
+library(rcompanion)
 
 medians <- read.delim(file="medians.tbl",sep="\t",header=FALSE)
 
-anova<-aov(V2~V1, data=medians)
-summary.aov(anova)
+# Normality test
+shapiro <- shapiro.test(medians$V2)
+print (shapiro)
 
-tukey<-TukeyHSD(anova)
-print(tukey)
+# Kruskal-Wallis test
+kw<-kruskal.test(V2~V1, data=medians)
+print(kw)
 
-cld<-multcompLetters4(anova,tukey)
+# Dunn's post-hoc test
+dunn <-dunnTest(V2~V1, data = medians, method = "bonferroni")
+print(dunn)
+
+# cld and boxplot
+cld <- cldList(comparison = dunn$res$Comparison, p.value = dunn$res$P.adj, threshold = 0.05) [1:2]
+names(cld)[1]<-"V1"
 print(cld)
 
-Tk <- group_by(medians, V1) %>%
-  summarise(mean=mean(V2), quant=quantile(V2, probs = 0.75)) %>%
-  arrange(desc(mean))
-
-cld <- as.data.frame.list(cld$V1)
-Tk$cld <- cld$Letters
-print(Tk)
-
-ggplot(medians,aes(x=factor(V1, level=c('Ty3', 'Pao', 'ERV', 'Copia', 'DIRS', 'L2', 'Rex', 'L1', 'RTE', 'I', 'SINE', 'TcMar', 'hAT', 'Harbinger', 'PiggyBac', 'EnSpm', 'Helitron', 'Polinton')),y=V2, fill = V1)) + 
+ggplot(data=medians,aes(x=factor(V1, level=c('Ty3', 'Pao', 'ERV', 'Copia', 'DIRS', 'L2', 'Rex', 'L1', 'RTE', 'I', 'SINE', 'TcMar', 'hAT', 'Harbinger', 'PiggyBac', 'EnSpm', 'Helitron', 'Polinton')),y=V2, fill = V1)) + 
   geom_boxplot(outlier.size = 0.2) +
   ylim(0, 0.3) +
   labs (x="Superfamilies", y="Median terminal branch length") +
   theme_bw() +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), text = element_text(size = 8), axis.text.x = element_text(angle = 90, vjust=0.3), legend.position="none") +
-  geom_text(data = Tk, aes(x = V1, y = quant, label = cld), size = 2, vjust=-1, hjust =-0.8, angle = 90) +
+  geom_text(data = cld, aes(x = V1, y = 0.3, label = Letter), size = 2, vjust=20, hjust =-0.8) +
   scale_fill_manual(values=c("#008B00", "#008B00", "#CD0000", "#008B00", "#CD0000", "#CD0000", "#CD0000", "#1874CD", "#1874CD", "#1874CD", "#008B00", "#CD0000", "#CD0000", "#1874CD", "#1874CD", "#00C5CD", "#CD0000", "#008B00")) +
   scale_x_discrete(labels=c("Ty3/Gypsy", "Bel-Pao", "ERV", "Copia", "DIRS", "L2", "Rex-Babar", "L1", "RTE", "I", "SINE", "Tc1-Mariner", "hAT", "PIF-Harbinger", "PiggyBac", "EnSpm", "Helitron", "Polinton"))
-                      
+
 ###########################################################
 # Relationship between medians and number of insertions
 
