94779576707539cd6b9276e2612c8f9b54136e82
Fernanda Souza de Oliveira <fersouzaoliveira@gmail.com>
2025-02-28 15:33:39 -0300

    Update anova_tukey.R

--- anova_tukey.R
+++ anova_tukey.R
@@ -24,11 +24,17 @@ cld <- as.data.frame.list(cld$V1)
 Tk$cld <- cld$Letters
 print(Tk)
 
-ggplot(gen_cov,aes(x=factor(V1, level=c('cs1', 'cs2', 'cs3', 'cs4', 'cs5', 'cs6', 'cs7', 'cs8', 'cs9', 'cs10', 'cs11', 'cs12', 'cs13', 'cs14', 'cs15', 'cs16', 'cs17', 'cs18', 'cs19', 'cs20', 'cs21', 'cs22', 'cs23', 'cs24', 'cs25', 'cs26', 'cs27', 'cs28')),y=V7)) + 
-  geom_boxplot(outlier.size = 0.2) +
-  ylim(0, 0.1) +
-  labs (x="Scaffold", y="coverage") +
-  theme_bw() +
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), text = element_text(size = 8)) +
-  geom_text(data = Tk, aes(x = V1, y = quant, label = cld), size = 2, vjust=-1, hjust =-0.8, angle = 90)
- 
+gen_cov$V2 <- gen_cov$V2/1000000
+
+ggplot(data=gen_cov, aes(x=V2,y=V7,fill=V1)) + 
+  geom_point(aes(x=V2,y=V7), size = 0.01) + 
+  facet_wrap(.~factor(V1, levels = c("cs1","cs2","cs3","cs4","cs5","cs6","cs7","cs8","cs9","cs10","cs11","cs12","cs13","cs14","cs15","cs16","cs17","cs18","cs19","cs20","cs21","cs22","cs23","cs24","cs25","cs26","cs27","cs28")), ncol=5, scales="free_x") +
+  ylim(0.00,0.50) +
+  theme(legend.position='none', 
+        text = element_text(size = 6),
+        axis.text = element_text(size=4.5), 
+        panel.spacing.y=unit(0.001, "lines"),
+        strip.background = element_blank(),
+        panel.border = element_rect(colour = "black", fill = NA)) +
+  labs(y="Genome Coverage", x="Scaffold Position (Mbp)") +
+  ggtitle("TE genome coverage")
